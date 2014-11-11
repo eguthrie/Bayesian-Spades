4171a0669af6251ab4dad21bd8bcbf8d4ac04d68
Emily Guthrie <you@example.com>
2014-11-11 11:25:07 -0500

    New awesome stuff

--- __weights__.py
+++ __weights__.py
@@ -1,5 +1,6 @@
 """This file contains code for use with the computational bayesian project
-of Emily Guthrie and Cameron Anderson.
+of Emily Guthrie and Cameron Anderson. 
+References and code: Allen Downey Think Bayesian and Think Python example code for card.py and thinkbayes2.py
 
 Write up of project: http://www.website.com
 """
@@ -12,9 +13,114 @@ Deck: represents a deck of cards probability mass function (map from values to p
 
 from thinkbayes2 import Pmf
 from thinkplot	 import *
+import random
 
-class Deck(Pmf):
-	"""represents a deck of cards' Pmf
-	Values can be any hashable type; probabilities are floating-point.
-	"""
 
+class Card(object):
+    """Represents a standard playing card.
+    
+    Attributes:
+      suit: integer 0-3
+      rank: integer 1-13
+    """
+
+    suit_names = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    rank_names = [None, "Ace", "2", "3", "4", "5", "6", "7", 
+              "8", "9", "10", "Jack", "Queen", "King"]
+
+    def __init__(self, suit=0, rank=2):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        """Returns a human-readable string representation."""
+        return '%s of %s' % (Card.rank_names[self.rank],
+                             Card.suit_names[self.suit])
+
+    def __cmp__(self, other):
+        """Compares this card to other, first by suit, then rank.
+
+        Returns a positive number if this > other; negative if other > this;
+        and 0 if they are equivalent.
+        """
+        t1 = self.suit, self.rank
+        t2 = other.suit, other.rank
+        return cmp(t1, t2)
+
+
+class Deck(object):
+    """Represents a deck of cards.
+
+    Attributes:
+      cards: list of Card objects.
+    """
+    def __init__(self):
+        self.cards = []
+        d = {}
+        for suit in range(4):
+            for rank in range(1, 14):
+            	if suit == 0:
+            		d[("spades",rank_names[self.rank])]= (53-rank)/52.0
+                card = Card(suit, rank)
+                self.cards.append(card)
+        print d
+
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
+
+    def add_card(self, card):
+        """Adds a card to the deck."""
+        self.cards.append(card)
+
+    def remove_card(self, card):
+        """Removes a card from the deck."""
+        self.cards.remove(card)
+
+    def pop_card(self, i=-1):
+        """Removes and returns a card from the deck.
+
+        i: index of the card to pop; by default, pops the last card.
+        """
+        return self.cards.pop(i)
+
+    def shuffle(self):
+        """Shuffles the cards in this deck."""
+        random.shuffle(self.cards)
+
+    def sort(self):
+        """Sorts the cards in ascending order."""
+        self.cards.sort()
+
+    def move_cards(self, hand, num):
+        """Moves the given number of cards from the deck into the Hand.
+
+        hand: destination Hand object
+        num: integer number of cards to move
+        """
+        for i in range(num):
+            hand.add_card(self.pop_card())
+
+class Hand(Deck):
+    """Represents a hand of playing cards."""
+    
+    def __init__(self, label=''):
+        self.cards = []
+        self.label = label
+        self.pmf=
+
+
+# def find_defining_class(obj, method_name):
+    """Finds and returns the class object that will provide 
+    the definition of method_name (as a string) if it is
+    invoked on obj.
+
+    obj: any python object
+    method_name: string method name
+    """
+#    for ty in type(obj).mro():
+#        if method_name in ty.__dict__:
+#            return ty
+#    return None
